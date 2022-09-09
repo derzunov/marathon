610ae0d22be0a82e388be0747633eee85efe1b17
derzunov <derzunov.work@gmail.com>
2022-09-09 21:42:06 +0300

    randomIndex hardcode to 3

--- 01-cards/app.js
+++ 01-cards/app.js
@@ -2,6 +2,9 @@ const slides = document.querySelectorAll('.slide')
 window.addEventListener('load', () => {
     // Как только страница загрузилась, делаем рандомную карточку активной
     let randomIndex = Math.round(Math.random() * (slides.length - 1))
+
+    // Но мне нравится больше всего слайд с железным человеком, так что активным делаем его =)
+    randomIndex = 3
     setTimeout(() => slides[randomIndex].classList.add('active'), 500)
 })
 
